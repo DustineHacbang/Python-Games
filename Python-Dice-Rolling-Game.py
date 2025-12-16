aeae57f97034da9113408ba7a6767d51f8540365
Dustine Hacbang <dustinehacbang526@gmail.com>
2025-12-16 07:28:58 -0800

    Add Python dice rolling game implementation

---
+++ Python-Dice-Rolling-Game.py
@@ -0,0 +1,20 @@
+# print("Hello World")
+import random
+value =input("Would you like to play the  python dice rolling game? (yes/no): ")
+if value == "yes":
+    while value == "yes":
+        dice_val = [1,2,3,4,5,6]
+        print (" Lets play the game")
+        print (" The dice is rolling...")
+        print (random.choice(dice_val))
+        value = input("Would you like to keep rolling? (yes/no): ")
+        if value == "no":
+            print (" Thank you for playing the game, come back soon!")
+            break
+        elif value != "yes":
+            print (" Please enter a valid response")
+            break
+elif value != "no":
+    print (" Please enter a valid response")
+else:
+    print (" Thank you for playing the game, come back soon!")
